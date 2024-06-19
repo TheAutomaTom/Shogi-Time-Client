fa90c028188ea7c8b11cc2df43827f7ff9974dec
Tom Grossi <TheAutomaTom@gmail.com>
2024-06-19 15:40:21 -0500

    Styling infrastructure

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.tjkmnsvuim8"
+    "revision": "0.i7o3ojoi8bo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
