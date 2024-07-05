106c399ec7e96d7bf76114061fc159f5486cb93e
Tom Grossi <TheAutomaTom@gmail.com>
2024-07-05 10:14:58 -0500

    Update square evaluator

--- src/State/Game/Movement/MobilityEngine.ts
+++ src/State/Game/Movement/MobilityEngine.ts
@@ -7,7 +7,7 @@ import { MoveStatus } from "./MoveStatus";
 
 export class MobilityEngine {
   
-  rebuild = async ( board: BoardModel ): Promise<BoardModel> =>{
+  rebuild = async ( player: number, board: BoardModel ): Promise<BoardModel> =>{
 
     let squares = [] as SquareModel[];
 
@@ -15,17 +15,14 @@ export class MobilityEngine {
     board.Squares.forEach( async square => {
       if(square.Piece.Player != 0){
         square.Piece.Mobility = await this.evaluateVectors(
+          player,
           board, 
           { X: square.Coordinate.X, Y: square.Coordinate.Y } as Coordinate,
           square.Piece.Range,
           square.Piece.IsFacingDefault
         )
-
-
-      }
-      
+      }      
     });
-
 
     let result = {
       Id: board.Id,
@@ -34,145 +31,9 @@ export class MobilityEngine {
     } as BoardModel;
     
     return result;
-
   };
-
   
-  evaluateVectors = async (
-    player: number, board: BoardModel, origin: Coordinate, range: PieceRange, isFacing: boolean
-  ): Promise<PieceMobility[]> => {
-
-    const facing = isFacing ? 1 : -1;   
-    let mobility = [] as PieceMobility[];
-    
-    // Process North ===============================================
-    for (let i = 1; i <= range.N; i++) {      
-      let target = { 
-        X: origin.X ,
-        Y: origin.Y + i * facing  
-      } as Coordinate;
-      mobility.push(await this.evaluateDestination( player, board, target ));
-    }
-    
-    // Process South ===============================================
-    hitObstacle = 0;
-    for (let i = 1; i <= range.S; i++) {
-      if(hitObstacle > 0) break;
-
-      let target = { 
-        X: origin.X ,
-        Y: origin.Y - i * facing
-      } as Coordinate;
-      hitObstacle = evaluateDestination(target);
-    }
-
-    // Process East ================================================
-    hitObstacle = 0;
-    for (let i = 1; i <= range.E; i++) {
-      if(hitObstacle > 0) break;
-
-      let target = { 
-        X: origin.X - i * facing ,
-        Y: origin.Y
-      } as Coordinate;
-      hitObstacle = evaluateDestination(target);
-    }
-    
-    // Process West ================================================
-    hitObstacle = 0;
-    for (let i = 1; i <= range.W; i++) {
-      if(hitObstacle > 0) break;
-
-      let target = { 
-        X: origin.X + i * facing ,
-        Y: origin.Y
-
-      } as Coordinate;
-      hitObstacle = evaluateDestination(target);
-    }
-
-    // Process North-West ==========================================
-    hitObstacle = 0;
-    for (let i = 1; i <= range.NW; i++) {
-      if(hitObstacle > 0) break;
-
-      let target = { 
-        X: origin.X + i * facing ,
-        Y: origin.Y + i * facing 
-
-      } as Coordinate;
-      hitObstacle = evaluateDestination(target);
-    }
-
-    // Process North-East ==========================================
-    hitObstacle = 0;
-    for (let i = 1; i <= range.NE; i++) {
-      if(hitObstacle > 0) break;
-
-      let target = { 
-        X: origin.X - i * facing ,
-        Y: origin.Y + i * facing 
-
-      } as Coordinate;
-      hitObstacle = evaluateDestination(target);
-    }
-
-    // Process South-East =========================================
-    hitObstacle = 0;
-    for (let i = 1; i <= range.SE; i++) {
-      if(hitObstacle > 0) break;
-
-      let target = { 
-        X: origin.X - i * facing ,
-        Y: origin.Y - i * facing 
-
-      } as Coordinate;
-      hitObstacle = evaluateDestination(target);
-    }
-
-    // Process South-West ==========================================
-    hitObstacle = 0;
-    for (let i = 1; i <= range.SW; i++) {
-      if(hitObstacle > 0) break;
-
-      let target = { 
-        X: origin.X + i * facing ,
-        Y: origin.Y - i * facing 
-
-      } as Coordinate;
-      hitObstacle = evaluateDestination(target);
-    }
-
-    // Process Knight ==============================================
-    if(range.K){
-      let target = { 
-        X: origin.X + 1 * facing,
-        Y: origin.Y + 2 * facing
-      } as Coordinate;
-
-      if(target.X > 0 && target.X < 10 && target.Y > 0 && target.Y < 10){
-        board.Squares.forEach( s => {
-          if(s.X == target.X && s.Y == target.Y)
-            PotentialDestinations.value.push(s.Id);
-        });
-      }
-
-      target = { 
-        X: origin.X + -1 * facing,
-        Y: origin.Y + 2 * facing
-      } as Coordinate;
-
-      if(target.X > 0 && target.X < 10 && target.Y > 0 && target.Y < 10){
-        board.Squares.forEach( s => {
-          if(s.X == target.X && s.Y == target.Y)
-            PotentialDestinations.value.push(s.Id);
-        });
-      }
-    }
-    return mobility;
-  };
-
-  evaluateDestination = async ( 
+  evaluateVector = async ( 
     player: number, board: BoardModel, target: Coordinate 
   ): Promise<PieceMobility> =>{
     
@@ -204,6 +65,113 @@ export class MobilityEngine {
 
   };
 
+  evaluateVectors = async (
+    player: number, board: BoardModel, origin: Coordinate, range: PieceRange, isFacing: boolean
+  ): Promise<PieceMobility[]> => {
+
+    const facing = isFacing ? 1 : -1;   
+    let mobility = [] as PieceMobility[];
+    
+    // Process North ===============================================
+    for (let i = 1; i <= range.N; i++) {      
+      let target = { 
+        X: origin.X ,
+        Y: origin.Y + i * facing  
+      } as Coordinate;
+      mobility.push(await this.evaluateVector( player, board, target ));
+    }
+    
+    // Process South ===============================================
+    for (let i = 1; i <= range.S; i++) {      
+      let target = { 
+        X: origin.X ,
+        Y: origin.Y - i * facing
+      } as Coordinate;
+      mobility.push(await this.evaluateVector( player, board, target ));
+    }
+
+    // Process East ================================================
+    for (let i = 1; i <= range.E; i++) {      
+      let target = { 
+        X: origin.X - i * facing ,
+        Y: origin.Y
+      } as Coordinate;
+      mobility.push(await this.evaluateVector( player, board, target ));
+    }
+      
+    // Process West ================================================
+    for (let i = 1; i <= range.W; i++) {      
+      let target = { 
+        X: origin.X + i * facing ,
+        Y: origin.Y
+      } as Coordinate;
+      mobility.push(await this.evaluateVector( player, board, target ));
+    }
+      
+    // Process North-West ==========================================
+    for (let i = 1; i <= range.NW; i++) {      
+      let target = { 
+        X: origin.X + i * facing ,
+        Y: origin.Y + i * facing 
+      } as Coordinate;
+      mobility.push(await this.evaluateVector( player, board, target ));
+    }
+      
+    // Process North-East ==========================================
+    for (let i = 1; i <= range.NE; i++) {      
+      let target = { 
+        X: origin.X - i * facing ,
+        Y: origin.Y + i * facing 
+      } as Coordinate;
+      mobility.push(await this.evaluateVector( player, board, target ));
+    }
+      
+    // Process South-East =========================================
+    for (let i = 1; i <= range.SE; i++) {      
+      let target = { 
+        X: origin.X - i * facing ,
+        Y: origin.Y - i * facing 
+      } as Coordinate;
+      mobility.push(await this.evaluateVector( player, board, target ));
+    }
+      
+    // Process South-West ==========================================
+    for (let i = 1; i <= range.SW; i++) {      
+      let target = { 
+        X: origin.X + i * facing ,
+        Y: origin.Y - i * facing 
+      } as Coordinate;
+      mobility.push(await this.evaluateVector( player, board, target ));
+    }
+      
+    // Process Knight ==============================================
+    if(range.K){
+      let target = { 
+        X: origin.X + 1 * facing,
+        Y: origin.Y + 2 * facing
+      } as Coordinate;
+
+      if(target.X > 0 && target.X < 10 && target.Y > 0 && target.Y < 10){
+        board.Squares.forEach( async s => {
+          if(s.Coordinate.X == target.X && s.Coordinate.Y == target.Y)
+            mobility.push(await this.evaluateVector( player, board, target ));
+        });
+      }
+
+      target = { 
+        X: origin.X + -1 * facing,
+        Y: origin.Y + 2 * facing
+      } as Coordinate;
+
+      if(target.X > 0 && target.X < 10 && target.Y > 0 && target.Y < 10){
+        board.Squares.forEach( async s => {
+          if(s.Coordinate.X == target.X && s.Coordinate.Y == target.Y)
+            mobility.push(await this.evaluateVector( player, board, target ));
+        });
+      }
+    }
+    return mobility;
+  };
 
 
 }
