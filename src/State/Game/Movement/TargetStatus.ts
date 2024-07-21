67adcf54fce1a450af482061cadf368f8b840405
Tom Grossi <TheAutomaTom@gmail.com>
2024-07-21 09:43:28 -0500

    Setup test board

--- src/State/Game/Movement/TargetStatus.ts
+++ src/State/Game/Movement/TargetStatus.ts
@@ -1,11 +1,16 @@
 export enum TargetStatus {
   Na = "na",
+  OutOfRange = "out-of-range",
+
   Open = "open",
   Blocked = "blocked",
   Ally = "ally",
   Enemy = "enemy",
+
   Pinned = "pinned",
   Check = "check",
-  OutOfRange = "out-of-range"
+
+  // BlockedCheck is used to retroactively scan for pinned pieces.
+  BlockedCheck = "blocked-check",
 
 }
